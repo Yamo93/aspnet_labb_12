c39dafc1273a17907ecbf31767d080f38ac37ee1
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2020-01-27 21:04:37 +0100

    Add project files.

---
+++ Labb12/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿// Funktion för att lägga till aktiv CSS-klass på det aktiva menyalternativet
+window.addEventListener('load', function () {
+    document.querySelectorAll('.navbar-item').forEach(function (element) {
+        if (element.href === window.location.href) {
+            element.parentElement.classList.add('active');
+        }
+    })
+});
